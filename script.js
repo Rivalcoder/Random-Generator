2778e81a2de87af98fda7f374453f30612b26cf7
Rivalcoder <divi242006@gmail.com>
2024-08-21 20:34:36 +0530

    random selector

---
+++ script.js
@@ -0,0 +1,22 @@
+function fun() {
+    const change = document.getElementById('out');
+    const area = document.getElementById('area');
+    const usertext = area.value;
+    const a=1;
+    console.log(usertext);
+    const combinedArray = usertext.split(/[\s\n]+/);
+    const len = combinedArray.length-1;
+
+    const b=Math.floor(Math.random() * (len - 0) + 0);
+    console.log(b);
+    console.log(len);
+    change.style.display = 'none';
+    if (change.style.display === 'none') {
+        alert("Processing....");
+        change.style.display = 'flex';
+        out.innerHTML=combinedArray[b];
+    } else {
+        alert("Error")
+        change.style.display = 'none'; 
+    }
+}
